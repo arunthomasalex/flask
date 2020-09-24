8832a62660bc8cbe5783ee7ee50cdc4fe0789308
arunthomasalex <arunthomasalex@gmail.com>
2020-09-24 10:35:03 +0530

    configuration logic change

--- config.py
+++ config.py
@@ -1,15 +1,6 @@
-import os
-
-DEBUG = False
-SECRET_KEY = 'lpXQ30UeNOV5SvHQ99WDm1TckXDtgC9b'
-# DATABASE = os.path.join(os.path.abspath(__name__), '..', 'instance', 'testcases.sqlite')
-# class Config(object):
-#     DEBUG = False
-#     SECRET_KEY = 'dev'
-#     DATABASE = os.path.join(os.path.abspath(__name__), '..', 'instance', 'testcases.sqlite')
-
-# class ProductionConfig(Config):
-#     DEBUG = False
-
-# class DevelopmentConfig(Config):
-#     DEBUG = True
+DEBUG = True
+SECRET_KEY = 'dev'
+DB = 'testcases'
+DB_HOST = 'localhost'
+DB_USERNAME = 'root'
+DB_PASSWORD = 'root'
--- testcases/db.py
+++ testcases/db.py
@@ -1,6 +1,13 @@
 import pymysql
+from config import ( DB_HOST, DB_USERNAME, DB_PASSWORD, DB,  )
 
 # from flask import current_app, g
 
 def get_connection():
-    return pymysql.connect(host='localhost', user='root', password='root', db='testcases', cursorclass=pymysql.cursors.DictCursor)
+    return pymysql.connect(
+        host = DB_HOST, 
+        user = DB_USERNAME, 
+        password = DB_PASSWORD, 
+        db = DB, 
+        cursorclass = pymysql.cursors.DictCursor
+    )
